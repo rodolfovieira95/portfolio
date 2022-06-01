038aa8de0c7ae7b1596865f704cae2be84ca616b
Rodolfo Vieira <rodolfovieira95@outlook.com>
2022-06-01 02:10:19 -0300

    feature: add some style to main component

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  display: flex;
+  h1,
+  h2 {
+    color: #08b6e1;
+  }
+  color: white;
+  h1 {
+    font-family: "Lato";
+    font-style: normal;
+    font-weight: 300;
+    font-size: 81.2308px;
+    line-height: 117px;
+    text-shadow: 0px 5.07692px 135.808px rgba(8, 182, 225, 0.3);
+    white-space: pre-line;
+  }
+`;
