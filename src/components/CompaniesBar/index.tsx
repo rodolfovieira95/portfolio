d63dd9ace35b7e4ed84bef39d13f5e0dba7e9b17
Rodolfo Vieira <rodolfovieira95@outlook.com>
2022-06-01 02:09:32 -0300

    feature: implements companies bar component

--- src/components/CompaniesBar/index.tsx
+++ src/components/CompaniesBar/index.tsx
@@ -1,3 +1,17 @@
+import Image from "next/image";
+import { CompaniesContainer } from "./style";
+import LogicalisLogo from "../../assets/logicalis-logo.png";
+import CaptalysLogo from "../../assets/captalys-logo.png";
+import AmigoEduLogo from "../../assets/amigoedu-logo.png";
+import PravalerLogo from "../../assets/pravaler-logo.png";
+
 export const CompaniesBar = () => {
-  return <div>CompaniesBar</div>;
+  return (
+    <CompaniesContainer>
+      <Image src={LogicalisLogo} alt="Logicalis logo" />
+      <Image src={CaptalysLogo} alt="Captalys logo"></Image>
+      <Image src={AmigoEduLogo} alt="AmigoEdu logo"></Image>
+      <Image src={PravalerLogo} alt="Pravaler logo"></Image>
+    </CompaniesContainer>
+  );
 };
---
+++ src/components/CompaniesBar/style.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const CompaniesContainer = styled.section`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 24px 0px;
+  background: #4ed0eb;
+  border: 6px solid #03a0c5;
+  gap: 200px;
+  height: 116px;
+`;
